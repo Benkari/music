79e8192dc12f223679bce45652c5ffc622ff78b9
Benjamin Karimaei <benjamin.karimaei@gmail.com>
2022-08-21 21:58:57 +0200

    Search Contaienr implemented

--- components/Header/CustomTextInput.js
+++ components/Header/CustomTextInput.js
@@ -2,9 +2,37 @@ import React from "react";
 import styles from "./CustomTextInput.module.css";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
+const useClickOutside = (handler) => {
+  const domNodeRef = React.useRef();
+
+  React.useEffect(() => {
+    const handleWorker = (event) => {
+      if (!domNodeRef.current.contains(event.target)) {
+        handler(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleWorker);
+
+    return () => {
+      document.removeEventListener("mousedown", handleWorker);
+    };
+  }, []);
+
+  return domNodeRef;
+};
+
 function CustomTextInput() {
+  const [searchActive, setSearchActive] = React.useState(false);
+
+  const domNodeRef = useClickOutside(setSearchActive);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={searchActive ? styles.containerActive : styles.container}
+      onClick={() => setSearchActive(true)}
+      ref={domNodeRef}
+    >
       <SearchOutlinedIcon color="gray" sx={{ fontSize: 18 }} />
       <form className={styles.form}>
         <input
@@ -13,8 +41,11 @@ function CustomTextInput() {
           name="fname"
           className={styles.textInput}
           placeholder="Search for songs, artist, lyrics"
+          autoComplete="off"
         />
       </form>
+      {searchActive && <div className={styles.betweenLine} />}
+      {searchActive && <div className={styles.searchResultsContainer}></div>}
     </div>
   );
 }
